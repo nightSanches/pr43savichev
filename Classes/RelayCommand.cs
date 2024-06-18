e56417d7baa4e2875fc1e13142909066d5fc320a
nightSanches <galesavich@gmail.com>
2024-06-18 19:14:59 +0500

    Добавьте файлы проекта.

---
+++ Classes/RelayCommand.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace pr43savichev.Classes
+{
+    public class RelayCommand
+    {
+        public class RealyCommand : ICommand
+        {
+            private Action<object> execute;
+            private Func<object, bool> canExecute;
+
+            public RealyCommand(Action<object> execute, Func<object, bool> canExecute = null)
+            {
+                this.canExecute = canExecute;
+            }
+
+            public event EventHandler CanExecuteChanged
+            {
+                add { CommandManager.RequerySuggested += value; }
+                remove { CommandManager.RequerySuggested -= value; }
+            }
+
+            public bool CanExecute(object parameter)
+            {
+                return this.canExecute == null || this.canExecute(parameter);
+            }
+
+            public void Execute(object parameter) => this.execute(parameter);
+        }
+    }
+}
---
+++ Models/Students.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pr43savichev.Models
+{
+    internal class Students
+    {
+    }
+}
