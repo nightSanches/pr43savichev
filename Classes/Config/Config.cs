3f7f0f74b75ba02054fdd9dcda4ba5fac18b2685
nightSanches <galesavich@gmail.com>
2024-06-18 19:20:01 +0500

    Реализованы модели, загружены библиотеки

---
+++ Classes/Config/Config.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pr43savichev.Classes.Config
+{
+    public class Config
+    {
+        public static readonly string connection = "server=127.0.0.1;port=3307;uid=root;pwd=;database=Base43;";
+        public static readonly MySqlServerVersion version = new MySqlServerVersion(new Version(8, 0, 11));
+    }
+}
